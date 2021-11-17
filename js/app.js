1c9d35119195e6f1c5ef0438c7c749549e025feb
Bruno Valsecchi <mbrunovalsecchi@gmail.com>
2021-11-17 15:43:25 -0300

    tercera entrega

--- js/app.js
+++ js/app.js
@@ -46,8 +46,49 @@ setInterval(function(){
 
 
 
+const slider2 = document.querySelector("#slider2");
+let sliderSection2 = document.querySelectorAll(".slider__section2");
+let sliderSectionLast2 = sliderSection2[sliderSection2.length -1];
 
+const btnLeft2 = document.querySelector("#btn-left2");
+const btnRight2 = document.querySelector("#btn-right2");
 
+slider2.insertAdjacentElement('afterbegin', sliderSectionLast2);
+
+function Next2() {
+  let sliderSectionFirst2 = document.querySelectorAll(".slider__section2")[0];
+  slider2.style.marginLeft = "-200%";
+  slider2.style.transition = "all 0.5s";
+  setTimeout2(function(){
+    slider2.style.transition = "none";
+    slider2.insertAdjacentElement('beforeend', sliderSectionFirst2);
+    slider2.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function Prev2() {
+  let sliderSection2 = document.querySelectorAll(".slider__section2");
+  let sliderSectionLast2 = sliderSection2[sliderSection2.length -1];
+  slider2.style.marginLeft = "0";
+  slider2.style.transition = "all 0.5s";
+  setTimeout2(function(){
+    slider2.style.transition = "none";
+    slider2.insertAdjacentElement('afterbegin', sliderSectionLast2);
+    slider2.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnRight2.addEventListener('click', function(){
+  Next2();
+});
+
+btnLeft2.addEventListener('click', function(){
+  Prev2();
+});
+
+setInterval2(function(){
+  Next2();
+}, 5000);
 
 /*
 const carrousel = document.querySelector("#carrousel");
